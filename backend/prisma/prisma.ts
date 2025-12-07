7db86bfb34b163583582a93d30c3ab8bb6fd25f3
TomWudo <tom.almarcha@wudo.io>
2025-12-07 11:32:07 +0100

    prisma working

--- backend/prisma/prisma.ts
+++ backend/prisma/prisma.ts
@@ -3,10 +3,10 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from './generated/prisma/client';
 
 const adapter = new PrismaMariaDb({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     connectionLimit: 10,
     connectTimeout: 30000,
 });
