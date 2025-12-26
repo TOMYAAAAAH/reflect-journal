64dbfd29fd27b9bf3ae4207b05c0b61994229b0e
TOMYAAAAAH <tom.almarcha@proton.me>
2025-12-26 01:26:47 +0100

    feat(day) front uses day route

--- frontend/src/pages/Today.tsx
+++ frontend/src/pages/Today.tsx
@@ -5,31 +5,22 @@ import AnswerInput from "../components/AnswerInput.tsx";
 
 export default function Today() {
 
-    const playlistId = 1;
 
-    const {data: questionData, isLoading: questionLoading, error: questionError} = useQuery({
-        queryKey: ['todayQuestion'],
-        queryFn: () => api(`/questions/today?playlist_id=${playlistId}`)
-    });
-
-    const {data: answerData, isLoading: answerLoading, error: answerError} = useQuery({
-        queryKey: ['todayAnswers'],
-        queryFn: () => api(`/answers/question/23`)
+    const {data: todayData, isLoading: todayLoading, error: todayError} = useQuery({
+        queryKey: ['today'],
+        queryFn: () => api(`/today`)
     });
 
     return (
         <>
 
-            {questionLoading && <p>Loading...</p>}
-            {questionError && <p>Error loading question</p>}
-            {questionData && (
-                <Question question={questionData.question}/>
-            )}
-
-            {answerLoading && <p>Loading...</p>}
-            {answerError && <p>Error loading question</p>}
-            {answerData && (
-                <AnswerInput answers={answerData.answers}/>
+            {todayLoading && <p>Loading...</p>}
+            {todayError && <p>Error loading question</p>}
+            {todayData && (
+                <>
+                <Question question={todayData.question}/>
+                <AnswerInput answers={todayData.answers}/>
+                </>
             )}
 
         </>
