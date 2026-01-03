98dcce5fef5572ea645b43b7a5174407846e0304
TOMYAAAAAH <tom.almarcha@proton.me>
2026-01-03 01:04:41 +0100

    wip front getting data

--- frontend/src/routes/Year.tsx
+++ frontend/src/routes/Year.tsx
@@ -1,10 +1,24 @@
 import {Link} from "react-router-dom";
 import getMonthFromNumber from "../utils/getMonthFromNumber.ts";
+import {useQuery} from "@tanstack/react-query";
+import {api} from "../api/client.ts";
 
 export default function Year() {
 
     const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+
+    const year = 2026;
+
+    const {data: calendarData, isLoading: calendarLoading, error: calendarError} = useQuery({
+        queryKey: ['calendar', year],
+        queryFn: () => api(`/calendar/${year}`)
+    });
+
+    if (calendarLoading) {<p>Loading...</p>}
+    if (calendarError) {<p>Error loading calendar</p>}
+
 
     const filledDays: Record<number, number[]> = {1: [1, 10, 31], 3: [10, 11, 12, 13, 14]};
 
@@ -35,6 +49,7 @@ export default function Year() {
                                                 <div className={'p-1'} key={day}>
                                                     {day}
                                                 </div>
+
 
 
                                             }
